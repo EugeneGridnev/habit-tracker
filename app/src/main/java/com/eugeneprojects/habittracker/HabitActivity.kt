9524520b67a6d1361fe681722ddcc52c71c6dd55
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-03 21:22:05 +0300

    initial habit priority spinner logic added

--- app/src/main/java/com/eugeneprojects/habittracker/HabitActivity.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/HabitActivity.kt
@@ -2,10 +2,27 @@ package com.eugeneprojects.habittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.eugeneprojects.habittracker.databinding.ActivityHabitBinding
+import com.eugeneprojects.habittracker.databinding.ActivityHabitListBinding
 
 class HabitActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHabitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_habit)
+        binding = ActivityHabitBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        setUpHabitPriority()
+    }
+
+    private fun setUpHabitPriority() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.habit_priority_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinHabitPriority.adapter = adapter
+        }
     }
 }
---
+++ app/src/main/java/com/eugeneprojects/habittracker/adapters/HabitPrioritySpinnerAdapter.kt
@@ -0,0 +1,14 @@
+package com.eugeneprojects.habittracker.adapters
+
+import android.view.View
+import android.widget.AdapterView
+
+class HabitPrioritySpinnerAdapter : AdapterView.OnItemSelectedListener {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+}
