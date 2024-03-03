665d74236a2d2b5b3a17f36c9d171101cf8d6870
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-03 22:00:21 +0300

    minor spinner change

--- app/src/main/java/com/eugeneprojects/habittracker/HabitActivity.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/HabitActivity.kt
@@ -2,6 +2,8 @@ package com.eugeneprojects.habittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.eugeneprojects.habittracker.databinding.ActivityHabitBinding
 
@@ -22,6 +24,15 @@ class HabitActivity : AppCompatActivity() {
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             binding.spinHabitPriority.adapter = adapter
+        }
+
+        binding.spinHabitPriority.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { }
+
         }
     }
 }
