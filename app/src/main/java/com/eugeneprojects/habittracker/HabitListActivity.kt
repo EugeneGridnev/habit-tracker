aa0fdcedb9559811e015a1199be262765f2deb8c
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-03 20:48:39 +0300

    fab button added

--- app/src/main/java/com/eugeneprojects/habittracker/HabitListActivity.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/HabitListActivity.kt
@@ -1,5 +1,6 @@
 package com.eugeneprojects.habittracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,12 +11,13 @@ class HabitListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHabitListBinding
 
-    lateinit var habitsAdapter: HabitsAdapter
+    private lateinit var habitsAdapter: HabitsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHabitListBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding = ActivityHabitListBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.fabAddHabit.setOnClickListener { onAddHabitFABPressed() }
+
         setUpRecyclerView()
     }
 
@@ -25,6 +27,11 @@ class HabitListActivity : AppCompatActivity() {
             adapter = habitsAdapter
             layoutManager = LinearLayoutManager(this@HabitListActivity)
         }
+    }
+
+    private fun onAddHabitFABPressed() {
+        val intent = Intent(this, HabitActivity::class.java)
+        startActivity(intent)
     }
 
 }
