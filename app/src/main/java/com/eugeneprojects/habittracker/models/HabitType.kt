22b71b266308fa5e4e2ca27c47b51961b780e5cb
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-05 20:54:05 +0300

    small model fix

--- app/src/main/java/com/eugeneprojects/habittracker/models/HabitType.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/models/HabitType.kt
@@ -1,9 +1,7 @@
 package com.eugeneprojects.habittracker.models
 
-import com.eugeneprojects.habittracker.R
-
 enum class HabitType(val string: String) {
-    NEUTRAL(R.string.radio_neutral.toString()),
-    GOOD(R.string.radio_good.toString()),
-    BAD(R.string.radio_bad.toString())
+    NEUTRAL("Нейтральная"),
+    GOOD("Хорошая"),
+    BAD("Плохая");
 }
