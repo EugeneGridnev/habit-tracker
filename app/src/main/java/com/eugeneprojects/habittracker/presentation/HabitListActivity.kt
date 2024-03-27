0cd2f5b39d68d3326d342c86c587a5babc2e67ca
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-27 22:21:16 +0300

    navigation drawer

--- app/src/main/java/com/eugeneprojects/habittracker/presentation/HabitListActivity.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/presentation/HabitListActivity.kt
@@ -3,6 +3,14 @@ package com.eugeneprojects.habittracker.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.eugeneprojects.habittracker.App
 import com.eugeneprojects.habittracker.R
@@ -11,11 +19,33 @@ import com.eugeneprojects.habittracker.databinding.ActivityHabitListBinding
 import com.eugeneprojects.habittracker.models.Habit
 import com.eugeneprojects.habittracker.models.HabitsListener
 import com.eugeneprojects.habittracker.models.HabitsService
+import com.google.android.material.navigation.NavigationView
 
 class HabitListActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHabitListBinding
+
+    private lateinit var listener: NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_habit_list)
+
+        binding = ActivityHabitListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.habits_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        binding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
