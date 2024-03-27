7f9eba4a589e70384ae46d80b67eff0f4b4b405d
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-27 13:10:05 +0300

    HabitFragment initial logic

--- app/src/main/java/com/eugeneprojects/habittracker/presentation/fragments/HabitFragment.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/presentation/fragments/HabitFragment.kt
@@ -1,60 +1,148 @@
 package com.eugeneprojects.habittracker.presentation.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import com.eugeneprojects.habittracker.App
 import com.eugeneprojects.habittracker.R
+import com.eugeneprojects.habittracker.databinding.FragmentHabitBinding
+import com.eugeneprojects.habittracker.models.Habit
+import com.eugeneprojects.habittracker.models.HabitPriority
+import com.eugeneprojects.habittracker.models.HabitType
+import com.eugeneprojects.habittracker.models.HabitsService
+import com.eugeneprojects.habittracker.presentation.HabitListActivity
+import java.lang.Exception
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HabitFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HabitFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentHabitBinding? = null
+    private lateinit var spinnerArrayAdapter: ArrayAdapter<HabitPriority>
+
+    private val habitsService: HabitsService
+        get() = (requireContext() as App).habitsService
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_habit, container, false)
+    ): View {
+        binding = FragmentHabitBinding.inflate(inflater)
+        return binding!!.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HabitFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HabitFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpUI()
     }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+
+    private fun setUpHabitPriority() {
+        spinnerArrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, HabitPriority.entries).also {
+                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding?.spinHabitPriority?.adapter = adapter
+        }
+
+        binding?.spinHabitPriority?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { }
+
+        }
+    }
+
+    //TODO: обрабатывать от состояния
+    private fun setUpUI() {
+        setUpHabitPriority()
+        if (habit.id == Habit.DEFAULT_ID) {
+            setUpSavedHabitButton()
+        } else {
+            setUpUIParams()
+            setUpUpdateHabitButton()
+        }
+
+    }
+
+    private fun setUpUIParams() {
+        binding?.edHabitName?.setText(habit.habitName)
+        binding?.edHabitDescription?.setText(habit.habitDescription)
+        when (habit.habitType) {
+            HabitType.NEUTRAL -> binding?.radioNeutral?.isChecked = true
+            HabitType.GOOD -> binding?.radioGood?.isChecked = true
+            HabitType.BAD -> binding?.radioBad?.isChecked = true
+        }
+        binding?.spinHabitPriority?.setSelection(spinnerArrayAdapter.getPosition(habit.habitPriority))
+        binding?.edHabitCount?.setText(habit.habitCount)
+        binding?.edHabitRhythm?.setText(habit.habitRhythm)
+    }
+
+    private fun setUpSavedHabitButton() {
+        binding?.btnHabitSave?.setOnClickListener { onSaveHabitPressed() }
+    }
+
+    private fun setUpUpdateHabitButton() {
+        binding?.btnHabitSave?.setOnClickListener { onUpdateHabitPressed() }
+    }
+
+    private fun onSaveHabitPressed() {
+        val intent = Intent(this, HabitListActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+        saveHabit()
+    }
+
+    private fun onUpdateHabitPressed() {
+        val intent = Intent(this, HabitListActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+        updateHabit()
+    }
+
+    private fun updateHabit() {
+        val id = habit.id
+        habitsService.updateHabit(
+            id,
+            binding?.edHabitName?.text.toString(),
+            binding?.edHabitDescription?.text.toString(),
+            spinnerArrayAdapter.getItem(binding?.spinHabitPriority?.selectedItemId!!.toInt())!!,
+            when (binding?.rgHabitType?.findViewById<RadioButton>(binding?.rgHabitType!!.checkedRadioButtonId)) {
+                binding?.radioNeutral -> HabitType.NEUTRAL
+                binding?.radioGood -> HabitType.GOOD
+                binding?.radioBad -> HabitType.BAD
+                else -> throw Exception()
+            },
+            binding?.edHabitCount?.text.toString(),
+            binding?.edHabitRhythm?.text.toString()
+        )
+    }
+
+    private fun saveHabit() {
+        val id = habitsService.getHabits().size
+        habitsService.addHabit(
+            id,
+            binding?.edHabitName?.text.toString(),
+            binding?.edHabitDescription?.text.toString(),
+            spinnerArrayAdapter.getItem(binding?.spinHabitPriority?.selectedItemId!!.toInt())!!,
+            when (binding?.rgHabitType?.findViewById<RadioButton>(binding?.rgHabitType!!.checkedRadioButtonId)) {
+                binding?.radioNeutral -> HabitType.NEUTRAL
+                binding?.radioGood -> HabitType.GOOD
+                binding?.radioBad -> HabitType.BAD
+                else -> throw Exception()
+            },
+            binding?.edHabitCount?.text.toString(),
+            binding?.edHabitRhythm?.text.toString()
+        )
+    }
+
 }
