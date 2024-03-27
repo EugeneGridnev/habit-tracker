babbec8532415bbd160e08206c3559a00a3bbda2
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-27 13:23:57 +0300

    HabitsListFragment initial logic

--- app/src/main/java/com/eugeneprojects/habittracker/presentation/fragments/HabitsListFragment.kt
+++ app/src/main/java/com/eugeneprojects/habittracker/presentation/fragments/HabitsListFragment.kt
@@ -1,60 +1,71 @@
 package com.eugeneprojects.habittracker.presentation.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.eugeneprojects.habittracker.App
 import com.eugeneprojects.habittracker.R
+import com.eugeneprojects.habittracker.adapters.HabitActionListener
+import com.eugeneprojects.habittracker.adapters.HabitsAdapter
+import com.eugeneprojects.habittracker.databinding.FragmentHabitsListBinding
+import com.eugeneprojects.habittracker.models.Habit
+import com.eugeneprojects.habittracker.models.HabitsListener
+import com.eugeneprojects.habittracker.models.HabitsService
+import com.eugeneprojects.habittracker.presentation.HabitActivity
+import com.eugeneprojects.habittracker.presentation.HabitListActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HabitsListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HabitsListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private var binding: FragmentHabitsListBinding? = null
+    private lateinit var habitsAdapter: HabitsAdapter
+
+    private val habitsListener: HabitsListener = {
+        habitsAdapter.habits = it
     }
+
+    private val habitsService: HabitsService
+        get() = (requireContext() as App).habitsService
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_habits_list, container, false)
+    ): View {
+        binding = FragmentHabitsListBinding.inflate(inflater)
+        return binding!!.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HabitsListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HabitsListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        habitsService.removeListener(habitsListener)
+        super.onDestroyView()
+    }
+
+    private fun setUpRecyclerView() {
+        //TODO: юзать лямбды
+        habitsAdapter = HabitsAdapter(object : HabitActionListener {
+            override fun onHabitClick(habit: Habit) {
+                val intent = Intent(this@HabitListActivity, HabitActivity::class.java)
+                intent.putExtra(HabitListActivity.HABIT_KEY, habit)
+                startActivity(intent)
             }
+
+        })
+        binding?.rvHabitsList?.apply {
+            adapter = habitsAdapter
+        }
+        habitsService.addListener (habitsListener)
+    }
+
+    private fun onAddHabitFABPressed() {
+        val intent = Intent(this, HabitActivity::class.java)
+        startActivity(intent)
     }
 }
